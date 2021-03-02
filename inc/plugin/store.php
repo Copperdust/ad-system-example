76e6eca1ed25ffdd6ae2c62e68b3bf3947dc946e
Fabio Pampin <fpampin@gm2dev.com>
2021-03-01 21:16:16 -0300

    Format store

--- inc/plugin/store.php
+++ inc/plugin/store.php
@@ -1,6 +1,7 @@
 <?php
 
-class ElementorAdSystemStore {
+class ElementorAdSystemStore
+{
   public static $globalProperties = array(
     'template',
     'countdown_year',
@@ -13,33 +14,36 @@ class ElementorAdSystemStore {
 
   public $id = false;
 
-  function __construct( $id ) {
+  function __construct($id)
+  {
     $this->id = $id;
   }
 
   /**
    * Magic getter to simplify this object
    */
-  function __get( $name ) {
-    if ( in_array( $name, self::$globalProperties ) ) {
-      return get_post_meta( $this->id, $name, true );
+  function __get($name)
+  {
+    if (in_array($name, self::$globalProperties)) {
+      return get_post_meta($this->id, $name, true);
     }
-    return new WP_Error('invalid_get_property', "The requested property '$name' does not exist for ".__CLASS__);
+    return new WP_Error('invalid_get_property', "The requested property '$name' does not exist for " . __CLASS__);
   }
 
-    /**
+  /**
    * Magic setter to simplify this object
    */
-
-  function __set( $name, $value ) {
-    if ( in_array( $name, self::$globalProperties ) ) {
-      return update_post_meta( $this->id, $name, $value );
+  function __set($name, $value)
+  {
+    if (in_array($name, self::$globalProperties)) {
+      return update_post_meta($this->id, $name, $value);
     }
-    return new WP_Error('invalid_set_property', "The requested property '$name' does not exist for ".__CLASS__);
+    return new WP_Error('invalid_set_property', "The requested property '$name' does not exist for " . __CLASS__);
   }
 
-  function save_all( $POST ) {
-    foreach ( self::$globalProperties as $prop ) {
+  function save_all($POST)
+  {
+    foreach (self::$globalProperties as $prop) {
       $this->{$prop} = $POST[$prop];
     }
   }
